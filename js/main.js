4fa1ee6fbb20d2dcb8269d121a06727388cb21f8
Nick Quebbeman <nick@kidqueb.com>
2012-09-10 22:38:15 -0500

    added form placeholders in index modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// homepage modal
+// modal
 $(function() {
-	$('a[rel*=leanModal]').leanModal({ top : 200, closeButton: ".modal_close" });
+	$('a[rel*=leanModal]').leanModal({ top : 200, overlay : 0.6, closeButton: ".modal_close" });
 });
