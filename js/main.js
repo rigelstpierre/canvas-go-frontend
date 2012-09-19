9b4e70b02ee84e9449e65a009645e6e829755760
Nick Quebbeman <nick@kidqueb.com>
2012-09-18 23:27:22 -0500

    added carousel

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 // modal
+
 $(function() {
 	$('a[rel*=leanModal]').leanModal({ top : 200, overlay : 0.6, closeButton: ".modal_close" });
+});
+
+
+
+
+
+// carousel
+
+$(document).ready(function(){
+	$('#slider').tinycarousel( { display: 4 } );
 });
