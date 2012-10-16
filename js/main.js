c7af1c5278012f86a2fad8232c95d65262a55387
Nick Quebbeman <nick@kidqueb.com>
2012-10-16 01:54:39 -0500

    im either a moron, or too tired to think of wtf to do for the calculating the total. will look at it again tomorrow

--- js/main.js
+++ js/main.js
@@ -22,6 +22,15 @@ $(document).ready(function(){
 // edit page active states
 // =====
 $(function() {
+
+	// calculate total
+	function calculateTotal() {
+		var borderCost = parseFloat( $('.addBorderColors.calc').children('.cost').html().replace("$", "") );
+			rolledCost = parseFloat( $('.addRolledCanvas.calc').children('.cost').html().replace("$", "") );
+			edgesCost = parseFloat( $('.addFancyEdges.calc').children('.cost').html().replace("$", "") );
+			total = borderCost.val(); + rolledCost.val(); + edgesCost.val();
+			alert(total)
+	}
 	
 	// effects
 	$('input[name="photoEffect"]').click(function() {
@@ -33,18 +42,47 @@ $(function() {
 	$('input[name="borderColors"]').click(function() {
 		$('input[name="borderColors"].active').removeClass('active');
 		$(this).addClass('active');
+		
+		var check = $(this).attr('class');
+			neg = 'none active'
+		
+		if ( check == neg ) {
+			$('.addBorderColors').hide();
+		} else {
+			$('.addBorderColors').show().addClass('calc');
+		}
+		
+		calculateTotal()
 	});
 	
 	// rolled canvas
 	$('input[name="rolledCanvas"]').click(function() {
 		$('input[name="rolledCanvas"].active').removeClass('active');
 		$(this).addClass('active');
+		
+		var check = $(this).attr('value');
+			neg = 'no'
+		
+		if ( check == neg ) {
+			$('.addRolledCanvas').hide();
+		} else {
+			$('.addRolledCanvas').show().addClass('calc');
+		}
 	});
 	
 	// fancy edges
 	$('input[name="fancyEdges"]').click(function() {
 		$('input[name="fancyEdges"].active').removeClass('active');
 		$(this).addClass('active');
+		
+		var check = $(this).attr('value');
+			neg = 'no'
+		
+		if ( check == neg ) {
+			$('.addFancyEdges').hide();
+		} else {
+			$('.addFancyEdges').show().addClass('calc');
+		}
 	});
 	
 	// canvas choice
